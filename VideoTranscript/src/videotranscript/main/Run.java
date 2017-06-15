e8e641dea6c8370631aa0b640f17fe4e0f46133b
Jim <Jim@Jim-THINK>
2017-06-15 11:58:40 -0400

    file input

--- VideoTranscript/src/videotranscript/main/Run.java
+++ VideoTranscript/src/videotranscript/main/Run.java
@@ -1,13 +1,20 @@
 package videotranscript.main;
 
 import java.io.File;
+import java.util.Scanner;
 
 import videotranscript.data.VideoToAudio;
 
 public class Run {
 
 	public static void main(String args[]) {
-		File Video = new File("C:\\Users\\Jim\\Desktop\\sample.mp4");
+		
+		//Ask for video file location
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Video File Location: ");
+		String vf = kb.nextLine();
+		//"C:\\Users\\Jim\\Desktop\\sample.mp4"
+		File Video = new File(vf);
 		VideoToAudio.ConvertToAudio(Video);
 	}
 }
